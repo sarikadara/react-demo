a6b7632c02362d51073f5faa622f58a0581b6f79
sarikadara <sarika.dara96@gmail.com>
2020-06-29 19:35:50 +0530

    a nd b

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import ReactDom from 'react-dom';
 
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {currentEvent: '___'}
-    this.update = this.update.bind(this)
+    this.state = {a: ''}
+    
 
   }
-  update(e) {
-    this.setState({currentEvent: e.type})
+  update() {
+    this.setState({
+      a: this.a.refs.input.value,
+      b: this.refs.b.value
+    })
   }
   render(){
     return (
      <div>
-       <textarea
-       onkeyPress={this.update}
-       onCut={this.update}
-       onCopy={this.update}
-       onPaste={this.update}
-       onFocus={this.update}
-       onBlur={this.update}
-       onDoubleClick={this.update}
-       onTouchStart={this.update}
-       onTouchMove={this.update}
-       onTouchEnd={this.update}
-
-        cols="30"
-        rows="10" />
-       <h1>{this.state.currentEvent}</h1>
-       
-       
-
+       <input
+        ref={ component=> this.a = this.component}
+        
+        update={this.update.bind(this)}
+       /> {this.state.a}
+       <hr />
+       <input
+        ref="b"
+        type="text"
+        onChange={this.update.bind(this)}
+       /> {this.state.b}
       </div>
     )
   }
 }
-
+class input extends React.Component {
+  render(){
+    return <div><input ref="input" type="text" onChange={this.props.update}/></div>
+  }
+}
 export default App;
